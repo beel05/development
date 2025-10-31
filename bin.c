17eeafd32b7e319c9b5058bbabbfe2d6037a9537
beel05 <25LBCS3106@culkomail.in>
2025-10-31 10:02:44 +0530

    Add files via upload

---
+++ bin.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void area_rect (float a, float b) ;
+void area_squ (float a) ;
+void area_cir (float b) ;
+
+int main () {
+    float a, b;
+    printf("Enter values: ");
+    scanf("%f %f", &a, &b);
+
+    area_rect(a, b);
+    area_cir(a);
+    area_squ(b);
+}
+
+void area_cir (float r) {
+    float area;
+    area = 2*3.14*r*r;
+    printf("Area of circle = %f", area);
+}
+
+void area_rect (float x, float y) {
+    float area;
+    area = x*y;
+    printf("Area of rectangle = %f", area);
+}
+
+void area_squ (float side) {
+    float area;
+    area = side*side;
+    printf("Area of square = %f", area);
+}
+
